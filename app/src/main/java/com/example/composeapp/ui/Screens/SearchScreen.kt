f5e2a772a6664511f2bd55741570da7c28acc723
Ketan Chaurasiya <113577637+KetanChaurasiya@users.noreply.github.com>
2022-11-07 16:33:51 +0530

    added changeable list scroll

--- app/src/main/java/com/example/composeapp/ui/Screens/SearchScreen.kt
+++ app/src/main/java/com/example/composeapp/ui/Screens/SearchScreen.kt
@@ -3,6 +3,7 @@ package com.example.composeapp.ui.Screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -11,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.composeapp.model.MagicDoor
 import com.example.composeapp.ui.components.MagicDoorCard
@@ -23,11 +23,14 @@ import kotlinx.coroutines.launch
 @Preview
 @Composable
 fun SearchScreen() {
-   val viewModel : SearchScreenViewModel = viewModel()
+    val viewModel: SearchScreenViewModel = viewModel()
     val coroutineScope = rememberCoroutineScope()
 
     val remotelist: MutableState<List<MagicDoor>> = remember {
         mutableStateOf(emptyList<MagicDoor>())
+    }
+    val isVerticalScroll: MutableState<Boolean> = remember {
+        mutableStateOf(true)
     }
     LaunchedEffect(key1 = "SearchScreen") {
         viewModel.getList()
@@ -37,44 +40,79 @@ fun SearchScreen() {
             }
         }
     }
-    Scaffold(topBar = {
-        TextButton(
-            modifier = Modifier
-                .height(40.dp)
-                .wrapContentSize(align = Alignment.Center),
-                //.weight(1f),
-            onClick = {
-                      viewModel.getList()
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Gray,
-                contentColor = Color.White
-            )
-        ) {
-            Text(text = "Reset")
-        }
-    },
+    Scaffold(
+        topBar = {
+            Row {
+                TextButton(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .wrapContentSize(align = Alignment.Center),
+                    //.weight(1f),
+                    onClick = {
+                        viewModel.getList()
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Gray,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(text = "Reset")
+                }
+                TextButton(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .wrapContentSize(align = Alignment.Center),
+                    //.weight(1f),
+                    onClick = {
+                        isVerticalScroll.value = !isVerticalScroll.value
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Gray,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(text = "Change Scroll Direction")
+                }
+            }
+        },
 
-    ) {
+        ) {
         Column(
             modifier = Modifier
-                .fillMaxSize().padding(it)
+                .fillMaxSize()
+                .padding(it)
                 .background(Color.Gray)
         ) {
-
-            LazyColumn(
-                modifier = Modifier
-                    .background(Color.Transparent)
-                    .fillMaxHeight()
-                    .align(Alignment.CenterHorizontally)
-                    .padding(start = 5.dp, end = 5.dp, bottom = 60.dp)
-            ) {
-                items(remotelist.value) { magicCard ->
-                    MagicDoorCard(magicDoor = magicCard){
-                        remotelist.value.find {item-> item==magicCard }?.isOpened=true
+            if (isVerticalScroll.value) {
+                LazyColumn(
+                    modifier = Modifier
+                        .background(Color.Transparent)
+                        .fillMaxHeight()
+                        .align(Alignment.CenterHorizontally)
+                        .padding(start = 5.dp, end = 5.dp, bottom = 60.dp)
+                ) {
+                    items(remotelist.value) { magicCard ->
+                        MagicDoorCard(magicDoor = magicCard) {
+                            remotelist.value.find { item -> item == magicCard }?.isOpened = true
+                        }
+                    }
+                }
+            } else {
+                LazyRow(
+                    modifier = Modifier
+                        .background(Color.Transparent)
+                        .fillMaxHeight()
+                        .align(Alignment.CenterHorizontally)
+                        .padding(start = 5.dp, end = 5.dp, bottom = 60.dp)
+                ) {
+                    items(remotelist.value) { magicCard ->
+                        MagicDoorCard(magicDoor = magicCard) {
+                            remotelist.value.find { item -> item == magicCard }?.isOpened = true
+                        }
                     }
                 }
             }
+
         }
     }
 
